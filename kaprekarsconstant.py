8d2632775a5f3a7349a0771ffa40912e14438c8d
abandomingo <abdomingo@csu.fullerton.edu>
2022-11-01 18:30:12 -0700

    Typo fixes

--- kaprekarsconstant.py
+++ kaprekarsconstant.py
@@ -1,12 +1,10 @@
-#kaprekar's constant
-#this constant is 6174
+#kaprekar's constant is 6174
 #you take any 4 digit number with at least 2 different digits (leading 0 are allowed)
 #arrange the digits in ascending order
 #and arrange the same num in descending order
 #subtract the two numbers 
 #and repeat until you end up with 6174
 
-""" REFACTORED FORMAT """
 def kaprekar(initialNum, count):
     initialList = strIntoList(initialNum)
     topNum, lowNum = sortLists(initialList)
@@ -14,12 +12,11 @@ def kaprekar(initialNum, count):
     initialNum, count = calculation(topInt, lowInt, count)
     formatText(topInt, lowInt, initialNum)
     checkResult(initialNum, count)
-
-  
+ 
 def strIntoList(initialNum):
     initialList = []
     for x in initialNum: #starts as a str
-      temp = int(x) #split the characters into a list and turn them into intergers
+      temp = int(x) #split the characters into a list and turn them into integers
       initialList.append(temp) #append each num in to a list [1,2,3,4]
     return initialList
 
@@ -41,9 +38,9 @@ def listIntoInt(topNum, lowNum):
     
 def calculation(topInt, lowInt, count):
     if topInt <= 1000:                          #if the top integer is less than 1000 we need to add a trailing zero 999 -> 9990
-      topInt *= 10                               #do this by muliply by 10
+      topInt *= 10                              #do this by muliplying by 10
     
-    initialNum = topInt - lowInt                 #subtract the two and make it our 'new' inital number
+    initialNum = topInt - lowInt                #subtract the two and make it our 'new' initial number
     count += 1                                  #up the count to know how many iterations it took
     return initialNum, count
 
@@ -52,12 +49,12 @@ def formatText(topInt, lowInt, initialNum):
 
 def checkResult(initialNum, count):
     if initialNum == 6174:
-      txt = "It took {0} iterations until you reached kaprekar's constant"
+      txt = "It took {0} iterations until you reached kaprekar's constant"  #we reached the const
       print(txt.format(count))
-    elif count > 50:
+    elif count > 50:                                                        #lazy stopper 
       print("The number may have been inputed wrong LOL ")
     else:
-      kaprekar(str(initialNum),count)
+      kaprekar(str(initialNum),count)                                       #recurse using new initial number and up the count
 
 
 if __name__ == '__main__':
